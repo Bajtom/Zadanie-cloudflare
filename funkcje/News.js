9f904905b185cfaef940015e416d910e14f7fa7f
Tomasz <114646591+Bajtom@users.noreply.github.com>
2023-04-20 12:18:27 +0200

    Update and rename getNews.js to News.js

---
+++ funkcje/News.js
@@ -0,0 +1,34 @@
+async function getNews(page, pageSize) {
+  const url = `https://newsapi.org/v2/top-headlines?country=us&page=${page}&pageSize=${pageSize}`;
+  const apiKey = CLOUDFLARE_ENV.API_KEY; // pobieramy klucz API z zmiennych środowiskowych
+
+  const response = await fetch(url, {
+    headers: {
+      "Authorization": `Bearer ${apiKey}`
+    }
+  });
+
+  const json = await response.json();
+
+  const news = json.articles.map((article) => ({
+    title: article.title,
+    description: article.description,
+    url: article.url
+  }));
+
+  return news;
+}
+
+// funkcja eksportująca, którą będzie można wywołać z zewnątrz
+export default async function(request) {
+  const page = request.query.get("page") || 1;
+  const pageSize = request.query.get("pageSize") || 10;
+
+  const news = await getNews(page, pageSize);
+
+  return new Response(JSON.stringify(news), {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+}
