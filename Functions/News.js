5e7a60009526bc1bb48506d3cd613d0ae74277cc
Tomasz <114646591+Bajtom@users.noreply.github.com>
2023-04-27 10:15:45 +0200

    .

--- Functions/News.js
+++ Functions/News.js
@@ -1,6 +1,10 @@
-export async function onRequestGet(page, pageSize) {
+export async function onRequestGet(context) {
+  const params = (new URL(context.request.url)).searchParams;
+
+    const page = params.get('pages');
+    const pagessize = params.get('pagessize');
   const url = `https://newsapi.org/v2/top-headlines?country=us&page=${page}&pageSize=${pageSize}`;
-  const apiKey = CLOUDFLARE_ENV.API_KEY; // pobieramy klucz API z zmiennych środowiskowych
+  const apiKey = context.env.API_KEY; // pobieramy klucz API z zmiennych środowiskowych
 
   const response = await fetch(url, {
     headers: {
@@ -16,19 +20,23 @@ export async function onRequestGet(page, pageSize) {
     url: article.url
   }));
 
-  return news;
-}
-
-// funkcja eksportująca, którą będzie można wywołać z zewnątrz
-export default async function(request) {
-  const page = request.query.get("page") || 1;
-  const pageSize = request.query.get("pageSize") || 10;
-
-  const news = await getNews(page, pageSize);
-
   return new Response(JSON.stringify(news), {
     headers: {
       "Content-Type": "application/json"
     }
   });
 }
+
+// funkcja eksportująca, którą będzie można wywołać z zewnątrz
+// export default async function(request) {
+//   const page = request.query.get("page") || 1;
+//   const pageSize = request.query.get("pageSize") || 10;
+
+//   const news = await getNews(page, pageSize);
+
+//   return new Response(JSON.stringify(news), {
+//     headers: {
+//       "Content-Type": "application/json"
+//     }
+//   });
+// }
